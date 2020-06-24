f1f309ac03762f699454bebe14a0f30377c86177
yuanjinghui <yuanjinghui718@hotmail.com>
2020-06-24 16:03:15 -0400

    initial

--- VDOTSmartRoadDataDownloader.py
+++ VDOTSmartRoadDataDownloader.py
@@ -9,17 +9,13 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import datetime
 from dateutil.relativedelta import relativedelta
+import time
+from lxml import etree as etree_lxml
+from io import StringIO, BytesIO
+import requests
 
 
-def dataDownloader(currentTime):
-
-    # get the start download time
-    # downloadStartTime = currentTime - datetime.timedelta(hours=1)
-    downloadStartTime = currentTime - relativedelta(years=3)
-    downloadStartTime = downloadStartTime.replace(day=1)
-    downloadStartTime = downloadStartTime.replace(hour=0)
-    downloadStartTime = downloadStartTime.replace(minute=0)
-
+def dataDownloader(downloadStartTime, currentTime):
     # get the end download time
     downloadEndTime = currentTime.replace(minute=0)
 
@@ -28,13 +24,15 @@ def dataDownloader(currentTime):
 
     global_month = downloadStartTime.month
     data = []
-    for time in timeRange:
-        print(time)
-        year = str(time)[0:4]
-        month = str(time)[5:7]
-        day = str(time)[8:10]
-        hours = str(time)[11:13]
-        minutes = str(time)[14:16]
+    for iterate_time in timeRange:
+        start = time.time()
+        # do some stuff
+        print(iterate_time)
+        year = str(iterate_time)[0:4]
+        month = str(iterate_time)[5:7]
+        day = str(iterate_time)[8:10]
+        hours = str(iterate_time)[11:13]
+        minutes = str(iterate_time)[14:16]
         print(year, month, day, hours, minutes)
 
         if int(month) > global_month:
@@ -47,26 +45,59 @@ def dataDownloader(currentTime):
 
         try:
             # read xml.gz files
-            if time < datetime.datetime.strptime('2017-12-13 08:23:00', '%Y-%m-%d %H:%M:%S'):
+            if iterate_time < datetime.datetime.strptime('2017-12-13 08:23:00', '%Y-%m-%d %H:%M:%S'):
+                # start = time.time()
 
-                testfile = urllib.request.URLopener()
-                testfile.retrieve(
-                    "https://smarterroads.org/dataset/download/1?token=18cPAKB4l5QucRdOHyOMi0EwI9ZRwWHuyaxhEXPpMvNzMtWIBooGp1GuzzqNsWI7&file=hub_data/TSSData/{}/{}/{}/{}/TSSData_{}{}{}_{}{}.xml.gz".
-                        format(year, month, day, hours, year, month, day, hours, minutes),
-                    "file.gz")
-                input = gzip.open('file.gz', 'r')
-                tree = ET.parse(input)
+                # testfile = urllib.request.URLopener()
+                # testfile.retrieve(
+                #     "https://smarterroads.org/dataset/download/1?token=18cPAKB4l5QucRdOHyOMi0EwI9ZRwWHuyaxhEXPpMvNzMtWIBooGp1GuzzqNsWI7&file=hub_data/TSSData/{}/{}/{}/{}/TSSData_{}{}{}_{}{}.xml.gz".
+                #         format(year, month, day, hours, year, month, day, hours, minutes),
+                #     "file.gz")
+                # input = gzip.open('file.gz', 'r')
+                # # tree = ET.parse(input)
+                # # root = tree.getroot()
+                #
+                # tree = etree_lxml.parse(input)
+                # root = tree.getroot()
+                #
+                file = urllib.request.urlopen("https://smarterroads.org/dataset/download/1?token=18cPAKB4l5QucRdOHyOMi0EwI9ZRwWHuyaxhEXPpMvNzMtWIBooGp1GuzzqNsWI7&file=hub_data/TSSData/{}/{}/{}/{}/TSSData_{}{}{}_{}{}.xml.gz".
+                        format(year, month, day, hours, year, month, day, hours, minutes))
+                input = gzip.open(file, 'r')
+                # tree = ET.parse(input)
+                # root = tree.getroot()
+                tree = etree_lxml.parse(input)
                 root = tree.getroot()
+
+                # stop = time.time()
+                # print(stop - start)
 
             # read xml files
             else:
-                testfile = urllib.request.URLopener()
-                testfile.retrieve(
-                    "https://smarterroads.org/dataset/download/1?token=18cPAKB4l5QucRdOHyOMi0EwI9ZRwWHuyaxhEXPpMvNzMtWIBooGp1GuzzqNsWI7&file=hub_data/TSSData/{}/{}/{}/{}/TSSData_{}{}{}_{}{}.xml".
-                        format(year, month, day, hours, year, month, day, hours, minutes),
-                    "file")
-                tree = ET.parse("file")
+                # start = time.time()
+                # testfile = urllib.request.URLopener()
+                # testfile.retrieve(
+                #     "https://smarterroads.org/dataset/download/1?token=18cPAKB4l5QucRdOHyOMi0EwI9ZRwWHuyaxhEXPpMvNzMtWIBooGp1GuzzqNsWI7&file=hub_data/TSSData/{}/{}/{}/{}/TSSData_{}{}{}_{}{}.xml".
+                #         format(year, month, day, hours, year, month, day, hours, minutes),
+                #     "file")
+                # tree = ET.parse("file")
+                # root = tree.getroot()
+
+                file = urllib.request.urlopen("https://smarterroads.org/dataset/download/1?token=18cPAKB4l5QucRdOHyOMi0EwI9ZRwWHuyaxhEXPpMvNzMtWIBooGp1GuzzqNsWI7&file=hub_data/TSSData/{}/{}/{}/{}/TSSData_{}{}{}_{}{}.xml".
+                        format(year, month, day, hours, year, month, day, hours, minutes))
+                tree = etree_lxml.parse(file)
                 root = tree.getroot()
+                # file = requests.get("https://smarterroads.org/dataset/download/1?token=18cPAKB4l5QucRdOHyOMi0EwI9ZRwWHuyaxhEXPpMvNzMtWIBooGp1GuzzqNsWI7&file=hub_data/TSSData/{}/{}/{}/{}/TSSData_{}{}{}_{}{}.xml".
+                #                     format(year, month, day, hours, year, month, day, hours, minutes))
+                # root = ET.fromstring(file.content)
+
+                # stop = time.time()
+                # print(stop - start)
+
+                # tree = ET.parse("file")
+                # root = tree.getroot()
+                # tree = etree_lxml.parse("file")
+                # root = tree.getroot()
+
         except:
             print('file read error')
             # data = pd.DataFrame(data)
@@ -85,18 +116,18 @@ def dataDownloader(currentTime):
                 for features in detector:
                     varName = features.tag
                     varName = varName.split("}")[-1]
-
                     # print(varName)
                     # print(features.text)
-                    if varName == 'the_geom':
-                        for i in features:
-                            for j in i:
-                                # var1 = j.tag
-                                # var1 = var1.split("}")[-1]
-                                # print(var1)
-                                # print(j.text)
-                                feature['long'] = j.text.split(" ")[0]
-                                feature['lat'] = j.text.split(" ")[1]
+                    # add coordinates information
+                    # if varName == 'the_geom':
+                    #     for i in features:
+                    #         for j in i:
+                    #             # var1 = j.tag
+                    #             # var1 = var1.split("}")[-1]
+                    #             # print(var1)
+                    #             # print(j.text)
+                    #             feature['long'] = j.text.split(" ")[0]
+                    #             feature['lat'] = j.text.split(" ")[1]
 
                     # print(features[''])
                     # for temp in features:
@@ -105,15 +136,30 @@ def dataDownloader(currentTime):
                     # print(features)
                 data.append(feature)
         global_month = int(month)
+
+        stop = time.time()
+        print(stop - start)
+
     data = pd.DataFrame(data)
     return data
 
 
-# time = datetime.datetime.strptime('2017-06-01 23:32:00', '%Y-%m-%d %H:%M:%S')
+# iterate_time = datetime.datetime.strptime('2017-12-13 07:22:00', '%Y-%m-%d %H:%M:%S')
+
 
 if __name__ == '__main__':
 
     currentTime = datetime.datetime.now().replace(microsecond=0)
-    data = dataDownloader(currentTime)
+
+    # get the start download time
+    # downloadStartTime = currentTime - datetime.timedelta(hours=1)
+    downloadStartTime = currentTime - relativedelta(years=3)
+    downloadStartTime = downloadStartTime.replace(day=1)
+    downloadStartTime = downloadStartTime.replace(hour=0)
+    downloadStartTime = downloadStartTime.replace(minute=0)
+    # start from July
+    downloadStartTime = downloadStartTime.replace(month=7)
+
+    data = dataDownloader(downloadStartTime, currentTime)
     data.to_csv('../../../data/yuanjinghui/VDOT_TSS_Data.csv')
 
